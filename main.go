ae251086327294692a07e7d8dd04706123ab8f74
Makavelli014 <kgdmitriev@gmail.com>
2024-08-01 22:02:22 +0300

    Запуск веб-сервера для snippetbox

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
-import "fmt"
+fmt
+)
 
-func main() {
-	var a string
-	var b string
-	fmt.Scanln(&a, &b)
-	fmt.Println("Привет", b)
-	fmt.Println("Как тебя зовут?", a)
+
+}
+func main	() {
+	tasks := make(chain int)
+	tasks <- 1
 }
