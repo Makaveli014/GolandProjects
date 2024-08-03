3882116e51e2215d35d17d6a344e6c2da3246ed4
Makavelli014 <kgdmitriev@gmail.com>
2024-08-03 15:33:19 +0300

    Обработка URL-запросов

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Обработчик главной страницы
@@ -20,12 +22,28 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Обработчик для отображения содержимого заметки.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Отображение заметки..."))
+	// Извлекаем значение параметра id из URL и попытаемся
+	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+	// конвертировать в integer, или значение меньше 1, возвращаем ответ
+	// 404 - страница не найдена!
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	// Используем функцию fmt.Fprintf() для вставки значения из id в строку ответа
+	// и записываем его в http.ResponseWriter.
+	fmt.Fprintf(w, "Отображение выбранно заметки с ID %d...", id)
 }
 
 // Обработчик для создания новой заметки.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+	// Используем r.Method для проверки, использует ли запрос метод POST или нет. Обратите внимание,
+	// что http.MethodPost является строкой и содержит текст "POST".
 	if r.Method != http.MethodPost {
+		// Используем метод Header().Set() для добавления заголовка 'Allow: POST' в
+		// карту HTTP-заголовков. Первый параметр - название заголовка, а
+		// второй параметр - значение заголовка.
 		w.Header().Set("Allow", http.MethodPost)
 
 		// Используем функцию http.Error() для отправки кода состояния 405 с соответствующим сообщением.
